c918f488741cbab24828152efb19b664c67b68b0
Nguyễn <tdinhnguyen279@gmail.com>
2022-05-13 13:32:49 +0700

    commit

--- src/navbar/Navigator.tsx
+++ src/navbar/Navigator.tsx
@@ -2,7 +2,7 @@ export const Navigator = ({key,text}:{key:number ;text:string})=>{
 
     return(
         <li className="font-serif">
-            <a href="a" className="hover:text-black hover:underline">
+            <a href="a" className="hover:text-black">
                 {text}
             </a>
         </li>
